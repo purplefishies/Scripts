2b28e78f7e3c08f128714cfbaf98ebc1d92c8b6e
Jimi Damon <jdamon@gmail.com>
2016-07-29 11:20:16 -0700

    ENH: keyring script

---
+++ Admin/keyring.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import gnomekeyring as gk
+import sys
+
+keyring = 'login'
+keyItems = gk.list_item_ids_sync(keyring)
+
+for keyItem in keyItems:
+    key = gk.item_get_info_sync(keyring, keyItem)
+    if  key.get_display_name() == sys.argv[1]:
+        # Your script here using key.get_secret()
+        print key.get_secret()
+
+
