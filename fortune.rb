034114cc2f0a56ec0c990e1f4b272ecd7586f4a0
Jimi Damon <jimi.damon@seescan.com>
2020-09-03 15:43:26 -0700

    :sparkles: Has better fixes for quote extraction

--- fortune.rb
+++ fortune.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+
 def get_files(dir)
   Dir.open(dir).find_all { |i|
     File.file?("#{dir}/#{i}") && i=~/^(.*?\.txt|[A-z]+)$/ 
@@ -11,10 +12,14 @@ def rand_quote(file)
   begin
     tmp = File.read(file).split(/^%/)
     
+    #while (((keep= tmp[0]) =~ /^[\s\n]*\Z/ms) == 0 )
     while (((keep= tmp.sample) =~ /^[\s\n]*\Z/ms) == 0 )
     end
-    # puts "KEEP: #{keep}"
-    keep.gsub!(/^[\s\n]+?(\[[^\]]+\])?[\s\n]*/s,'')
+    #puts "KEEP: #{keep}"
+    #binding.pry
+    #puts "HERE"
+    keep.gsub!(/^[\s\n]*?(\[[^\]]+\])?[\s\n]*/s,'')
+    #puts "KEEP: #{keep}"
     keep = "\n" + keep + "\n"
     return keep
   rescue Exception => e
